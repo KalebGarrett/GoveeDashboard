45d80e306fe514f921744fbdf3ca4a74a64ab1a8
Kaleb Garrett <kalebagarrett@gmail.com>
2024-03-14 18:31:12 -0400

    Added Govee service

--- GoveeDashboard.App/Program.cs
+++ GoveeDashboard.App/Program.cs
@@ -1,11 +1,13 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using GoveeDashboard.App;
+using GoveeDashboard.App.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
+builder.Services.AddScoped<GoveeService>();
 
 await builder.Build().RunAsync();
---
+++ GoveeDashboard.App/Services/GoveeService.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json;
+using GoveeDashboard.App.Secrets;
+using GoveeDashboard.Models;
+
+namespace GoveeDashboard.App.Services;
+
+public class GoveeService
+{
+    private readonly HttpClient _httpClient;
+
+    public GoveeService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<List<Device>> Get()
+    {
+        _httpClient.DefaultRequestHeaders.Add("Govee-API-Key", Govee.ApiKey);
+        var result = await _httpClient.GetAsync("https://openapi.api.govee.com/router/api/v1/user/devices");
+        if (!result.IsSuccessStatusCode) return new List<Device>();
+        var json = await result.Content.ReadAsStringAsync();
+        var content = JsonSerializer.Deserialize<List<Device>>(json);
+        Console.WriteLine(content);
+        return content;
+    }
+}
